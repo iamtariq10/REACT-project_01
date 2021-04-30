9cdc76168b1c23d80d181e08b65b4e31fb52394c
Mohammad Tariq <m.faixal16212@gmail.com>
2021-04-30 10:30:25 +0500

    Dashboard UI

---
+++ project_01/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import PersistentDrawerLeft from './Components/drawer';
+import CustomizedMenus from './Components/openMenu'
+
+function App() {
+  return (
+    <div className="App">
+
+     <PersistentDrawerLeft/>
+     
+    </div>
+  );
+}
+
+export default App;
